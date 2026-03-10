153aa7641b76dac2d60c70fbf35fe96750474e88
Avi Sharma <118112869+avi-i05@users.noreply.github.com>
2026-03-10 20:16:27 +0530

    Add new allowed origin for socket connections

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 
 let io;
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174","https://test-admin-frontend-lake.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174","https://test-admin-frontend-lake.vercel.app","https://test-partner-frontend.vercel.app"];
 
 
 export const initSocket = (server) => {
